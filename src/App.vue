d7a22fcdad9e9a1e11b2f431dc3bb22fc75192cf
md-arif-islam <arifislamarif344@gmail.com>
2023-08-04 08:57:42 +0600

    UPDATED: Added Bootstrap CSS and implemented Navbar and PageViewer components
    - Added Bootstrap CSS link in the head of index.html
    - Created Navbar component with dynamic navigation links and theme toggle functionality
    - Created PageViewer component to display page content

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <p>simple text</p>
+  <navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => (activePage = index)"
+  ></navbar>
+
+  <page-viewer :page="pages[activePage]"></page-viewer>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
+import PageViewer from "./components/PageViewer.vue";
+
 export default {
+  components: {
+    Navbar,
+    PageViewer,
+  },
   data() {
     return {
       activePage: 0,
---
+++ src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <nav :class="[`navbar-${theme}`, `bg-${theme}`, `navbar navbar-expand-lg`]">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Vue 3</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto">
+            <li
+              v-for="(page, index) in pages"
+              class="nav-item active"
+              :key="index"
+            >
+              <a
+                class="nav-link"
+                :class="{ active: activePage == index }"
+                :title="`This link goes to ${page.link.text} page`"
+                :href="page.link.url"
+                @click.prevent="navLinkClick(index)"
+                >{{ page.link.text }}
+              </a>
+            </li>
+          </ul>
+
+          <form class="d-flex">
+            <button class="btn btn-primary" @click.prevent="changeTheme()">
+              Toggle
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["pages", "activePage", "navLinkClick"],
+
+  data() {
+    return {
+      theme: "light",
+    };
+  },
+
+  methods: {
+    changeTheme() {
+      let theme = "light";
+
+      if (this.theme == "light") {
+        theme = "dark";
+      }
+      this.theme = theme;
+    },
+  },
+};
+</script>

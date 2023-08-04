c0a0d78ba12a341af7c971a96959c78daf2375c1
md-arif-islam <arifislamarif344@gmail.com>
2023-08-04 21:38:54 +0600

    AD link text and link URL were added to.vue template. - Added two additional form fields for link text and link URL inPage.vue template.
    ADDED: Computed property isFormInvalid was added to check if the form is invalid.
    - Added a computed property called isFormInvalid that checks if any of the form fields (pageTitle, pageContent, linkText, linkUrl) are empty.
    
    ADDED: Method submitForm was added to handle form submission.
    - Added a method called submitForm that checks if all form fields are filled out and calls the pageCreated function with the form data.
    
    FIXED: The button for submitting the form is now disabled if the form is invalid.

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -1,26 +1,47 @@
 <template>
   <div class="container mb-3">
     <form action="">
-      <div class="mb-3">
-        <label for="" class="form-label"> Page Title </label>
-        <input type="text" class="form-control" v-model="pageTitle" />
-      </div>
-      <div class="mb-3">
-        <label for="" class="form-label"> Content main </label>
-        <textarea
-          type="text"
-          class="form-control"
-          rows="5"
-          v-model="pageContent"
-        ></textarea>
-      </div>
-      <div class="mb-3">
-        <button
-          class="btn btn-primary"
-          @click.prevent="pageCreated({ pageTitle, pageContent })"
-        >
-          Create Page
-        </button>
+      <div class="row">
+        <div class="col-8">
+          <div class="mb-3">
+            <label for="" class="form-label"> Page Title </label>
+            <input type="text" class="form-control" v-model="pageTitle" />
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label"> Content main </label>
+            <textarea
+              type="text"
+              class="form-control"
+              rows="5"
+              v-model="pageContent"
+            ></textarea>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="mb-3">
+            <label for="" class="form-label"> Link Text </label>
+            <input type="text" class="form-control" v-model="linkText" />
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label"> Link Url </label>
+            <input type="text" class="form-control" v-model="linkUrl" />
+          </div>
+          <div class="mb-3">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" />
+              <label for="" class="form-check-label">Publish</label>
+            </div>
+          </div>
+        </div>
+        <div class="mb-3">
+          <button
+            :disabled="isFormInvalid"
+            class="btn btn-primary"
+            @click.prevent="submitForm"
+          >
+            Create Page
+          </button>
+        </div>
       </div>
     </form>
   </div>
@@ -29,11 +50,42 @@
 <script>
 export default {
   props: ["pageCreated"],
+  computed: {
+    isFormInvalid() {
+      return (
+        !this.pageTitle || !this.pageContent || !this.linkText || !this.linkUrl
+      );
+    },
+  },
   data() {
     return {
       pageTitle: "",
       pageContent: "",
+      linkText: "",
+      linkUrl: "",
     };
+  },
+  methods: {
+    submitForm(e) {
+      if (
+        !this.pageTitle ||
+        !this.pageContent ||
+        !this.linkText ||
+        !this.linkUrl
+      ) {
+        alert("Please fill the form!");
+        return;
+      }
+
+      this.pageCreated({
+        pageTitle: this.pageTitle,
+        pageContent: this.pageContent,
+        link: {
+          linkText: this.linkText,
+          linkUrl: this.linkUrl,
+        },
+      });
+    },
   },
 };
 </script>
